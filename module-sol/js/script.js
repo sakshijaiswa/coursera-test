d3191315259b485070e1991e63e5ac4c20f0043d
sakshijaiswa <sakshi.jaiswal2019@vitstudent.ac.in>
2020-07-20 16:04:20 +0530

    'pages'

--- module-sol/js/script.js
+++ module-sol/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       
-      var chosenCategoryShortName = "'"+ categories[chooseRandomCategory(categories)].short_name+"'";
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       
-      var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml,'randomCategoryShortName',chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml,"randomCategoryShortName","'" + chosenCategoryShortName + "'");
       insertHtml('#main-content',homeHtmlToInsertIntoMainPage);
 
 
